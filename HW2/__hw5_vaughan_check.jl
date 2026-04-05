78b2dd0e444f3b56d5ed5512915552d0f03bade3
Yuxuan <yuxuanzhao.econ@gmail.com>
2026-04-04 21:11:14 -0500

    update

---
+++ HW2/__hw5_vaughan_check.jl
@@ -0,0 +1,3 @@
+﻿include("C:/Users/30945/Desktop/Yuxuan ZHAO/minnesota/PhD_first_year/ECON8208/ECON8208Tools.jl")
+using .ECON8208Tools
+println(hasmethod(ECON8208Tools.solve_vaughan, Tuple{Any,Any,Any,Any,Any,Any}))
